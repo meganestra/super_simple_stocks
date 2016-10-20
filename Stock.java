3267666c9d7b64f03f7e83b2c5c6d636dc3157c7
meganestra <m.e.strachan@gmail.com>
2016-10-20 13:58:30 +0100

    initial commit

---
+++ Stock.java
@@ -0,0 +1,26 @@
+public class Stock {
+
+	String ticker;
+	String type;
+	int lastDividend;
+	int fixedDividend;
+	int parValue;
+	double price;
+	boolean callable;
+	double votingRights;
+
+	public Stock(String ticker, String type, int lastDividend, int fixedDividend, int parValue, double price, boolean callable, double votingRights){
+		this.ticker = ticker;
+		this.type = type;
+		this.lastDividend = lastDividend;
+		this.fixedDividend = fixedDividend;
+		this.parValue = parValue;
+		this.price = price;
+		this.callable = callable;
+		this.votingRights = votingRights;
+	}
+
+
+
+
+}
